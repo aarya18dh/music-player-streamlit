d3c8721b25ad67e9a096fdb738ef64758b894ad8
aarya18dh <156456841+aarya18dh@users.noreply.github.com>
2025-09-30 19:42:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import streamlit.components.v1 as components
-import requests
 
 st.set_page_config(page_title="Music Player", layout="centered")
 
-# Fetch HTML file from GitHub repo (raw link)
-url = "https://raw.githubusercontent.com/<aarya18dh>/music-player-streamlit/main/music_player.html"
-html_code = requests.get(url).text
+# Load the local HTML file
+with open("music_player.html", "r", encoding="utf-8") as f:
+    html_code = f.read()
 
 components.html(html_code, height=720, scrolling=True)
